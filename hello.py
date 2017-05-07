b55534edb0350fbbf7b94f8cb6bd2180344f3e7c
Tsung Chun Hsu <osoo20906@gmail.com>
2017-05-07 23:34:52 +0000

    first commite

---
+++ hello.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+    
+    
+if __name__ == '__main__':
+    host = os.getenv('IP', '0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
+    app.run(host = host, port = port)
